e0e1930569d16734870969c182919a9bf12378b6
Saad Ahmad <saad1912@gmail.com>
2025-03-02 17:44:05 +0530

    requirements.txt installed

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "src",
+    version = "0.0.1",
+    author="Saad Ahmad",
+    author_email="saad1912@gmail",
+    packages=find_packages()
+)
